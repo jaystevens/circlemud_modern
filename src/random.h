4fc45c37cec54e3e261330f9889f509621ba8e19
Jason Stevens <jay@wizardofthenet.com>
2019-08-18 23:56:16 -0700

    add random.h

---
+++ src/random.h
@@ -0,0 +1,11 @@
+//
+// Created by root on 8/18/19.
+//
+
+#ifndef CIRCLEMUD_RANDOM_H
+#define CIRCLEMUD_RANDOM_H
+
+void circle_srandom(unsigned long initial_seed);
+unsigned long circle_random(void);
+
+#endif //CIRCLEMUD_RANDOM_H
